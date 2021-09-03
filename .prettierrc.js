61acf90f29e67cd6c5465e5fe1ed1e7fbe0f3e29
Vincent Ip <vincent@qomorebi.net>
2021-09-03 17:44:52 +0200

    prettier, eslint, formatting and typescript

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "all",
+  arrowParens: "always",
+};
--- src/index.js
+++ src/index.js
@@ -1,46 +1,46 @@
-const util = require("util");
-const PDFDocument = require("pdf-lib").PDFDocument;
-const fs = require("fs");
-const path = require("path");
+const util = require('util')
+const PDFDocument = require('pdf-lib').PDFDocument
+const fs = require('fs')
+const path = require('path')
 
-const readdir = util.promisify(fs.readdir);
+const readdir = util.promisify(fs.readdir)
 
 async function merge() {
-  const directoryPath = path.join(__dirname, "../pdf");
-  let pdfFiles;
+  const directoryPath = path.join(__dirname, '../pdf')
+  let pdfFiles
 
   try {
-    pdfFiles = await readdir(directoryPath);
+    pdfFiles = await readdir(directoryPath)
   } catch (e) {
-    console.error(e);
+    console.error(e)
   }
 
-  const pdfsToMerge = [];
+  const pdfsToMerge = []
   for (const file of pdfFiles) {
-    if (file.endsWith("pdf")) {
-      pdfsToMerge.push(fs.readFileSync(path.join(__dirname, `../pdf/${file}`)));
+    if (file.endsWith('pdf')) {
+      pdfsToMerge.push(fs.readFileSync(path.join(__dirname, `../pdf/${file}`)))
     }
   }
-  console.log(pdfsToMerge.length);
-  const mergedPdf = await PDFDocument.create();
+  console.log(pdfsToMerge.length)
+  const mergedPdf = await PDFDocument.create()
   for (const pdfBytes of pdfsToMerge) {
-    const pdf = await PDFDocument.load(pdfBytes);
-    const copiedPages = await mergedPdf.copyPages(pdf, pdf.getPageIndices());
+    const pdf = await PDFDocument.load(pdfBytes)
+    const copiedPages = await mergedPdf.copyPages(pdf, pdf.getPageIndices())
     copiedPages.forEach((page) => {
-      mergedPdf.addPage(page);
-    });
+      mergedPdf.addPage(page)
+    })
   }
 
-  const buf = await mergedPdf.save(); // Uint8Array
+  const buf = await mergedPdf.save() // Uint8Array
 
-  let fileName = "merged.pdf";
-  fs.open(fileName, "w", function (err, fd) {
+  let fileName = 'merged.pdf'
+  fs.open(fileName, 'w', function (err, fd) {
     fs.write(fd, buf, 0, buf.length, null, function (err) {
       fs.close(fd, function () {
-        console.log("wrote the file successfully");
-      });
-    });
-  });
+        console.log('wrote the file successfully')
+      })
+    })
+  })
 }
 
-(async () => await merge())();
+;(async () => await merge())()
